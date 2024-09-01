ebeb0ede6b21f0059adcbd07d6e1b9f42c94e617
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-09-02 00:13:38 +0100

    deleted some stuff

--- backend/shop/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import InventoryItem, Sold
-# Register your models here.
-
-admin.site.register(InventoryItem)
-admin.site.register(Sold)
-
--- backend/workers/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AtrisansConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'workers'
