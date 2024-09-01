5989d6b0c58923298d2bc63ddb42a3ffe90f1f63
Masodi Success <109852676+suskidee@users.noreply.github.com>
2024-09-02 00:20:02 +0100

    changed some stuff

--- backend/products/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Product, Quotation, RawMaterialUsed
-# Register your models here.
-
-admin.site.register(Product)
-admin.site.register(Quotation)
-admin.site.register(RawMaterialUsed)
-
--- backend/store/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import RawMaterial, Removed
-# Register your models here.
-admin.site.register(RawMaterial)
-admin.site.register(Removed)
-
